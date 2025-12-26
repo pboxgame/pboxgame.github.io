2fdff5e84fbfef27c3174f49f93580f7e39384fe
justvaclav <stramgraminc@gmail.com>
2025-12-26 08:40:56 +0100

    first commit

---
+++ ediths0.16.php
@@ -0,0 +1,26 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>интерфейс</title>
+    <link rel='stylesheet' href='style.css'>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <style>
+        a:visited {
+            color: #3300FF;
+        }
+    </style>
+</head>
+<body>
+    <?php include "header.html"; ?>
+    <?php include "ediths0.16.html"; ?>
+    <?php include "footer.html"; ?>
+</body>
+</html>
+
+
+
